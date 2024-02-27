2f799b10d066d95cc1736c035be7b6852895cd8a
Achat <l.s.schulz@gmx.de>
2024-02-27 09:57:36 +0100

    Added resizing to framebuffer

--- Nebula/Src/Rendering/Buffer/Framebuffer.cs
+++ Nebula/Src/Rendering/Buffer/Framebuffer.cs
@@ -4,34 +4,39 @@ namespace Nebula.Rendering;
 
 internal class Framebuffer : IDisposable
 {
-    private readonly uint r_handle;
-    private readonly uint r_colourAttachement;
-    private readonly uint r_rbo;
+    private uint m_handle;
+    private uint m_colourAttachement;
+    private uint m_rbo;
 
-    public unsafe Framebuffer(Vector2i size)
+    public Framebuffer(Vector2i size)
     {
-        r_handle = GL.Get().GenFramebuffer();
+        Recreate(size);
+    }
+
+    private unsafe void Recreate(Vector2i size)
+    {
+        m_handle = GL.Get().GenFramebuffer();
         Bind();
 
         // Create colour attachement
-        r_colourAttachement = GL.Get().GenTexture();
-        GL.Get().BindTexture(TextureTarget.Texture2D, r_colourAttachement);
+        m_colourAttachement = GL.Get().GenTexture();
+        GL.Get().BindTexture(TextureTarget.Texture2D, m_colourAttachement);
         GL.Get().TexImage2D(TextureTarget.Texture2D, 0, InternalFormat.Rgb, (uint)size.X, (uint)size.Y, 0, PixelFormat.Rgb, PixelType.UnsignedByte, null);
         GL.Get().TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)Texture.FilterMode.Linear);
         GL.Get().TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)Texture.FilterMode.Linear);
         GL.Get().BindTexture(TextureTarget.Texture2D, 0);
 
-        GL.Get().FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, r_colourAttachement, 0);
+        GL.Get().FramebufferTexture2D(FramebufferTarget.Framebuffer, FramebufferAttachment.ColorAttachment0, TextureTarget.Texture2D, m_colourAttachement, 0);
 
         // Create render buffer object
         // | This includes a depth and a stencil attachment in one buffer
         // | But it is writeonly
-        r_rbo = GL.Get().GenRenderbuffer();
-        GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, r_rbo);
+        m_rbo = GL.Get().GenRenderbuffer();
+        GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, m_rbo);
         GL.Get().RenderbufferStorage(RenderbufferTarget.Renderbuffer, InternalFormat.Depth24Stencil8, (uint)size.X, (uint)size.Y);
         GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, 0);
 
-        GL.Get().FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthStencilAttachment, RenderbufferTarget.Renderbuffer, r_rbo);
+        GL.Get().FramebufferRenderbuffer(FramebufferTarget.Framebuffer, FramebufferAttachment.DepthStencilAttachment, RenderbufferTarget.Renderbuffer, m_rbo);
 
         if (GL.Get().CheckFramebufferStatus(FramebufferTarget.Framebuffer) != GLEnum.FramebufferComplete)
         {
@@ -39,11 +44,13 @@ internal class Framebuffer : IDisposable
         }
 
         Unbind();
+
+        Window.Resizing += OnResize;
     }
 
     internal void Bind()
     {
-        GL.Get().BindFramebuffer(FramebufferTarget.Framebuffer, r_handle);
+        GL.Get().BindFramebuffer(FramebufferTarget.Framebuffer, m_handle);
     }
 
     internal void Unbind()
@@ -51,13 +58,22 @@ internal class Framebuffer : IDisposable
         GL.Get().BindFramebuffer(FramebufferTarget.Framebuffer, 0);
     }
 
+    private void OnResize(Vector2i size)
+    {
+        Dispose();
+        Recreate(size);
+    }
+
     public uint GetColourAttachment()
     {
-        return r_colourAttachement;
+        return m_colourAttachement;
     }
 
     public void Dispose()
     {
-        GL.Get().DeleteFramebuffer(r_handle);
+        GL.Get().DeleteTexture(m_colourAttachement);
+        GL.Get().DeleteRenderbuffer(m_rbo);
+        GL.Get().DeleteFramebuffer(m_handle);
+        Window.Resizing -= OnResize;
     }
 }

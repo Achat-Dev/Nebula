87242c7ab27d73828648e94075cdecca549adb2e
Achat <l.s.schulz@gmx.de>
2024-03-10 12:03:15 +0100

    Added skybox creation via skybox config

--- Nebula/Src/Rendering/Skybox.cs
+++ Nebula/Src/Rendering/Skybox.cs
@@ -7,14 +7,14 @@ public class Skybox : IDisposable
     private readonly Cubemap r_prefilteredMap;
     private readonly Texture r_brdfLutHandle;
 
-    public Skybox(Texture hdrTexture)
+    public Skybox(Texture hdrTexture, SkyboxConfig config)
     {
-        r_environmentMap = Cubemap.Create(hdrTexture, Cubemap.CubemapType.Skybox, new Vector2i(512, 512));
-        r_irradianceMap = Cubemap.Create(r_environmentMap, Cubemap.CubemapType.Irradiance, new Vector2i(32, 32));
-        r_prefilteredMap = Cubemap.Create(r_environmentMap, Cubemap.CubemapType.Prefiltered, new Vector2i(128, 128));
+        r_environmentMap = Cubemap.Create(hdrTexture, Cubemap.CubemapType.Skybox, config.EnvironmentMapSize);
+        r_irradianceMap = Cubemap.Create(r_environmentMap, Cubemap.CubemapType.Irradiance, config.IrradianceMapSize);
+        r_prefilteredMap = Cubemap.Create(r_environmentMap, Cubemap.CubemapType.Prefiltered, config.PrefilteredMapSize);
         TextureConfig brdfLutConfig = TextureConfig.DefaultHdr;
         brdfLutConfig.GenerateMipMaps = false;
-        r_brdfLutHandle = Texture.CreateFromCapture(Shader.Create("Shader/Brdf.vert", "Shader/Brdf.frag", false), new Vector2i(512, 512), brdfLutConfig);
+        r_brdfLutHandle = Texture.CreateFromCapture(Shader.Create("Shader/Brdf.vert", "Shader/Brdf.frag", false), config.EnvironmentMapSize, brdfLutConfig);
     }
 
     internal Cubemap GetEnvironmentMap()

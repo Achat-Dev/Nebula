4e7c15ff6bac31d256f180cc3b86f6ce80d8ca02
Achat <l.s.schulz@gmx.de>
2024-03-24 14:48:24 +0100

    Added texture arrays as framebuffer attachments

--- Nebula/Src/Rendering/Buffer/FramebufferAttachment.cs
+++ Nebula/Src/Rendering/Buffer/FramebufferAttachment.cs
@@ -13,88 +13,47 @@ internal class FramebufferAttachment : IDisposable, ITextureBindable
 
     public enum TextureType
     {
-        Texture = 0,
-        Cubemap = 1,
-        Renderbuffer = 2,
-
-        Readable = 0,
-        Writeonly = 2,
+        Texture,
+        TextureArray,
+        Cubemap,
+        CubemapArray,
+        Renderbuffer,
     }
 
     private readonly uint r_handle;
 
     private readonly AttachmentType r_attachmentType;
     private readonly TextureType r_textureType;
+    private readonly uint r_arraySize;
     private readonly InternalFormat r_internalFormat;
     private readonly PixelFormat r_pixelFormat;
     private readonly PixelType r_pixelType;
 
-    internal unsafe FramebufferAttachment(FramebufferAttachmentConfig config, Vector2i size)
+    internal FramebufferAttachment(FramebufferAttachmentConfig config, Vector2i size)
     {
         r_attachmentType = config.AttachmentType;
         r_textureType = config.TextureType;
+        r_arraySize = config.ArraySize;
         r_internalFormat = config.GetInternalFormat();
         r_pixelFormat = config.GetPixelFormat();
         r_pixelType = config.GetPixelType();
 
-        int wrapMode = (int)config.WrapMode;
         switch (r_textureType)
         {
             case TextureType.Texture:
-                r_handle = GL.Get().GenTexture();
-                Bind(Texture.Unit.Texture0);
-
-                GL.Get().TexImage2D(TextureTarget.Texture2D, 0, r_internalFormat, (uint)size.X, (uint)size.Y, 0, r_pixelFormat, r_pixelType, null);
-
-                GL.Get().TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, wrapMode);
-                GL.Get().TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, wrapMode);
-                GL.Get().TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)config.MinFilterMode);
-                GL.Get().TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)config.MaxFilterMode);
-
-                if (config.GenerateMipMaps)
-                {
-                    GL.Get().TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureBaseLevel, 0);
-                    GL.Get().TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMaxLevel, config.MaxMipMapLevel);
-                    GL.Get().GenerateMipmap(TextureTarget.Texture2D);
-                }
-
-                GL.Get().BindTexture(TextureTarget.Texture2D, 0);
-                GL.Get().FramebufferTexture2D(FramebufferTarget.Framebuffer, config.GetSilkAttachment(), TextureTarget.Texture2D, r_handle, 0);
+                r_handle = GenerateTexture(config, size);
+                break;
+            case TextureType.TextureArray:
+                r_handle = GenerateTextureArray(config, size);
                 break;
             case TextureType.Cubemap:
-                r_handle = GL.Get().GenTexture();
-                Bind(Texture.Unit.Texture0);
-
-                for (int i = 0; i < 6; i++)
-                {
-                    GL.Get().TexImage2D(TextureTarget.TextureCubeMapPositiveX + i, 0, r_internalFormat, (uint)size.X, (uint)size.Y, 0, r_pixelFormat, r_pixelType, null);
-                }
-
-                GL.Get().TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapS, wrapMode);
-                GL.Get().TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapT, wrapMode);
-                GL.Get().TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapR, wrapMode);
-
-                GL.Get().TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureMinFilter, (int)config.MinFilterMode);
-                GL.Get().TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureMagFilter, (int)config.MaxFilterMode);
-
-                if (config.GenerateMipMaps)
-                {
-                    GL.Get().TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureBaseLevel, 0);
-                    GL.Get().TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureMaxLevel, config.MaxMipMapLevel);
-                    GL.Get().GenerateMipmap(TextureTarget.TextureCubeMap);
-                }
-
-                GL.Get().BindTexture(TextureTarget.TextureCubeMap, 0);
-                GL.Get().FramebufferTexture(FramebufferTarget.Framebuffer, config.GetSilkAttachment(), r_handle, 0);
+                r_handle = GenerateCubemap(config, size);
+                break;
+            case TextureType.CubemapArray:
+                r_handle = GenerateCubemapArray(config, size);
                 break;
             case TextureType.Renderbuffer:
-                r_handle = GL.Get().GenRenderbuffer();
-
-                GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, r_handle);
-                GL.Get().RenderbufferStorage(RenderbufferTarget.Renderbuffer, r_internalFormat, (uint)size.X, (uint)size.Y);
-                GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, 0);
-
-                GL.Get().FramebufferRenderbuffer(FramebufferTarget.Framebuffer, config.GetSilkAttachment(), RenderbufferTarget.Renderbuffer, r_handle);
+                r_handle = GenerateRenderbuffer(config, size);
                 break;
         }
     }
@@ -108,6 +67,14 @@ internal class FramebufferAttachment : IDisposable, ITextureBindable
                 GL.Get().TexImage2D(TextureTarget.Texture2D, 0, r_internalFormat, (uint)size.X, (uint)size.Y, 0, r_pixelFormat, r_pixelType, null);
                 GL.Get().BindTexture(TextureTarget.Texture2D, 0);
                 break;
+            case TextureType.TextureArray:
+                GL.Get().BindTexture(TextureTarget.Texture2DArray, r_handle);
+                for (uint i = 0; i < r_arraySize; i++)
+                {
+                    GL.Get().TexImage3D(TextureTarget.Texture2DArray, 0, r_internalFormat, (uint)size.X, (uint)size.Y, i, 0, r_pixelFormat, r_pixelType, null);
+                }
+                GL.Get().BindTexture(TextureTarget.Texture2DArray, 0);
+                break;
             case TextureType.Cubemap:
                 GL.Get().BindTexture(TextureTarget.TextureCubeMap, r_handle);
                 for (int i = 0; i < 6; i++)
@@ -116,12 +83,106 @@ internal class FramebufferAttachment : IDisposable, ITextureBindable
                 }
                 GL.Get().BindTexture(TextureTarget.TextureCubeMap, 0);
                 break;
+            case TextureType.CubemapArray:
+                GL.Get().BindTexture(TextureTarget.TextureCubeMapArray, r_handle);
+                uint faceCount = r_arraySize * 6;
+                for (uint i = 0; i < faceCount; i++)
+                {
+                    GL.Get().TexImage3D(TextureTarget.TextureCubeMapArray, 0, r_internalFormat, (uint)size.X, (uint)size.Y, i, 0, r_pixelFormat, r_pixelType, null);
+                }
+                GL.Get().BindTexture(TextureTarget.TextureCubeMapArray, 0);
+                break;
             case TextureType.Renderbuffer:
                 GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, r_handle);
                 GL.Get().RenderbufferStorage(RenderbufferTarget.Renderbuffer, r_internalFormat, (uint)size.X, (uint)size.Y);
                 GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, 0);
                 break;
         }
+    }
+
+    private unsafe uint GenerateTexture(FramebufferAttachmentConfig config, Vector2i size)
+    {
+        uint handle = GL.Get().GenTexture();
+        GL.Get().BindTexture(TextureTarget.Texture2D, handle);
+
+        GL.Get().TexImage2D(TextureTarget.Texture2D, 0, r_internalFormat, (uint)size.X, (uint)size.Y, 0, r_pixelFormat, r_pixelType, null);
+
+        Utils.TextureUtils.SetBaseTextureParameters(TextureTarget.Texture2D, config);
+
+        GL.Get().BindTexture(TextureTarget.Texture2D, 0);
+        GL.Get().FramebufferTexture2D(FramebufferTarget.Framebuffer, config.GetSilkAttachment(), TextureTarget.Texture2D, handle, 0);
+
+        return handle;
+    }
+
+    private unsafe uint GenerateTextureArray(FramebufferAttachmentConfig config, Vector2i size)
+    {
+        uint handle = GL.Get().GenTexture();
+        GL.Get().BindTexture(TextureTarget.Texture2DArray, handle);
+
+        for (uint i = 0; i < config.ArraySize; i++)
+        {
+            GL.Get().TexImage3D(TextureTarget.Texture2DArray, 0, r_internalFormat, (uint)size.X, (uint)size.Y, i, 0, r_pixelFormat, r_pixelType, null);
+        }
+
+        Utils.TextureUtils.SetBaseTextureParameters(TextureTarget.Texture2DArray, config);
+
+        GL.Get().BindTexture(TextureTarget.Texture2DArray, 0);
+        GL.Get().FramebufferTexture(FramebufferTarget.Framebuffer, config.GetSilkAttachment(), handle, 0);
+
+        return handle;
+    }
+
+    private unsafe uint GenerateCubemap(FramebufferAttachmentConfig config, Vector2i size)
+    {
+        uint handle = GL.Get().GenTexture();
+        GL.Get().BindTexture(TextureTarget.TextureCubeMap, handle);
+
+        for (int i = 0; i < 6; i++)
+        {
+            GL.Get().TexImage2D(TextureTarget.TextureCubeMapPositiveX + i, 0, r_internalFormat, (uint)size.X, (uint)size.Y, 0, r_pixelFormat, r_pixelType, null);
+        }
+
+        Utils.TextureUtils.SetBaseTextureParameters(TextureTarget.TextureCubeMap, config);
+        GL.Get().TexParameter(TextureTarget.TextureCubeMap, TextureParameterName.TextureWrapR, (int)config.WrapMode);
+
+        GL.Get().BindTexture(TextureTarget.TextureCubeMap, 0);
+        GL.Get().FramebufferTexture(FramebufferTarget.Framebuffer, config.GetSilkAttachment(), handle, 0);
+
+        return handle;
+    }
+
+    private unsafe uint GenerateCubemapArray(FramebufferAttachmentConfig config, Vector2i size)
+    {
+        uint handle = GL.Get().GenTexture();
+        GL.Get().BindTexture(TextureTarget.TextureCubeMapArray, handle);
+
+        uint faceCount = config.ArraySize * 6;
+        for (uint i = 0; i < faceCount; i++)
+        {
+            GL.Get().TexImage3D(TextureTarget.TextureCubeMapArray, 0, r_internalFormat, (uint)size.X, (uint)size.Y, i, 0, r_pixelFormat, r_pixelType, null);
+        }
+
+        Utils.TextureUtils.SetBaseTextureParameters(TextureTarget.TextureCubeMapArray, config);
+        GL.Get().TexParameter(TextureTarget.TextureCubeMapArray, TextureParameterName.TextureWrapR, (int)config.WrapMode);
+
+        GL.Get().BindTexture(TextureTarget.TextureCubeMapArray, 0);
+        GL.Get().FramebufferTexture(FramebufferTarget.Framebuffer, config.GetSilkAttachment(), handle, 0);
+
+        return handle;
+    }
+
+    private unsafe uint GenerateRenderbuffer(FramebufferAttachmentConfig config, Vector2i size)
+    {
+        uint handle = GL.Get().GenRenderbuffer();
+
+        GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, handle);
+        GL.Get().RenderbufferStorage(RenderbufferTarget.Renderbuffer, r_internalFormat, (uint)size.X, (uint)size.Y);
+        GL.Get().BindRenderbuffer(RenderbufferTarget.Renderbuffer, 0);
+
+        GL.Get().FramebufferRenderbuffer(FramebufferTarget.Framebuffer, config.GetSilkAttachment(), RenderbufferTarget.Renderbuffer, handle);
+
+        return handle;
     }
 
     public void Bind(Texture.Unit textureUnit)
@@ -138,8 +199,14 @@ internal class FramebufferAttachment : IDisposable, ITextureBindable
             case TextureType.Texture:
                 GL.Get().BindTexture(TextureTarget.Texture2D, r_handle);
                 break;
+            case TextureType.TextureArray:
+                GL.Get().BindTexture(TextureTarget.Texture2DArray, r_handle);
+                break;
             case TextureType.Cubemap:
                 GL.Get().BindTexture(TextureTarget.TextureCubeMap, r_handle);
+                break;
+            case TextureType.CubemapArray:
+                GL.Get().BindTexture(TextureTarget.TextureCubeMapArray, r_handle);
                 break;
         }
     }
@@ -161,17 +228,13 @@ internal class FramebufferAttachment : IDisposable, ITextureBindable
 
     void IDisposable.Dispose()
     {
-        switch (r_textureType)
+        if (r_textureType == TextureType.Renderbuffer)
         {
-            case TextureType.Texture:
-                GL.Get().DeleteTexture(r_handle);
-                break;
-            case TextureType.Cubemap:
-                GL.Get().DeleteTexture(r_handle);
-                break;
-            case TextureType.Renderbuffer:
-                GL.Get().DeleteRenderbuffer(r_handle);
-                break;
+            GL.Get().DeleteRenderbuffer(r_handle);
+        }
+        else
+        {
+            GL.Get().DeleteTexture(r_handle);
         }
     }
 }
--- Nebula/Src/Rendering/Buffer/FramebufferAttachmentConfig.cs
+++ Nebula/Src/Rendering/Buffer/FramebufferAttachmentConfig.cs
@@ -4,6 +4,7 @@ internal class FramebufferAttachmentConfig : TextureConfigBase
 {
     public FramebufferAttachment.AttachmentType AttachmentType;
     public FramebufferAttachment.TextureType TextureType;
+    public uint ArraySize;
 
     public class Defaults
     {
@@ -12,6 +13,7 @@ internal class FramebufferAttachmentConfig : TextureConfigBase
             return new FramebufferAttachmentConfig(
                 FramebufferAttachment.AttachmentType.Colour,
                 FramebufferAttachment.TextureType.Texture,
+                0,
                 Texture.Format.Rgb,
                 Texture.DataType.UnsignedByte,
                 Texture.WrapMode.ClampToEdge,
@@ -25,6 +27,7 @@ internal class FramebufferAttachmentConfig : TextureConfigBase
             return new FramebufferAttachmentConfig(
                 FramebufferAttachment.AttachmentType.Depth,
                 FramebufferAttachment.TextureType.Renderbuffer,
+                0,
                 Texture.Format.Depth,
                 Texture.DataType.Float,
                 Texture.WrapMode.ClampToEdge,
@@ -38,6 +41,7 @@ internal class FramebufferAttachmentConfig : TextureConfigBase
             return new FramebufferAttachmentConfig(
                 FramebufferAttachment.AttachmentType.DepthStencil,
                 FramebufferAttachment.TextureType.Renderbuffer,
+                0,
                 Texture.Format.DepthStencil,
                 Texture.DataType.UnsignedInt248,
                 Texture.WrapMode.ClampToEdge,
@@ -49,17 +53,17 @@ internal class FramebufferAttachmentConfig : TextureConfigBase
 
     private FramebufferAttachmentConfig() { }
 
-    public FramebufferAttachmentConfig(FramebufferAttachment.AttachmentType attachmentType, FramebufferAttachment.TextureType readWriteMode, Texture.Format format, Texture.DataType dataType, Texture.WrapMode wrapMode, Texture.FilterMode filterMode, bool generateMipMaps, int maxMipMapLevel)
-        : this(attachmentType, readWriteMode, format, dataType, wrapMode, filterMode, filterMode, generateMipMaps, maxMipMapLevel)
+    public FramebufferAttachmentConfig(FramebufferAttachment.AttachmentType attachmentType, FramebufferAttachment.TextureType textureType, uint arraySize, Texture.Format format, Texture.DataType dataType, Texture.WrapMode wrapMode, Texture.FilterMode filterMode, bool generateMipMaps, int maxMipMapLevel)
+        : this(attachmentType, textureType, arraySize, format, dataType, wrapMode, filterMode, filterMode, generateMipMaps, maxMipMapLevel)
     {
 
     }
 
-    public FramebufferAttachmentConfig(FramebufferAttachment.AttachmentType attachmentType, FramebufferAttachment.TextureType readWriteMode, Texture.Format format, Texture.DataType dataType, Texture.WrapMode wrapMode, Texture.FilterMode minFilterMode, Texture.FilterMode maxFilterMode, bool generateMipMaps, int maxMipMapLevel)
+    public FramebufferAttachmentConfig(FramebufferAttachment.AttachmentType attachmentType, FramebufferAttachment.TextureType textureType, uint arraySize, Texture.Format format, Texture.DataType dataType, Texture.WrapMode wrapMode, Texture.FilterMode minFilterMode, Texture.FilterMode maxFilterMode, bool generateMipMaps, int maxMipMapLevel)
     : base(format, dataType, wrapMode, minFilterMode, maxFilterMode, generateMipMaps, maxMipMapLevel)
     {
         AttachmentType = attachmentType;
-        TextureType = readWriteMode;
+        TextureType = textureType;
     }
 
     internal Silk.NET.OpenGL.FramebufferAttachment GetSilkAttachment()

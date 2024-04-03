bfcdfc9063f09611c5ab191e172564e967e59557
Achat <l.s.schulz@gmx.de>
2024-04-03 15:05:26 +0200

    Fixed directional shadow calcualtion displaying wrong values outside of directinal shadow view distance

--- Nebula/Assets/Shader/Include/Math/Shadows/DirectionalShadowValue.glsl
+++ Nebula/Assets/Shader/Include/Math/Shadows/DirectionalShadowValue.glsl
@@ -5,6 +5,11 @@
 
 	float mappedDepth = texture(u_directionalShadowMap, uv.xy).r;
 
+	if (uv.z > 1.0)
+	{
+		return 1.0;
+	}
+
 	if (mappedDepth < uv.z - c_directionalShadowSamplingBias)
 	{
 		if (max(uv.x, uv.y) > 1.0 || min(uv.x, uv.y) < 0.0)
